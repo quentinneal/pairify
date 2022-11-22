9dd38ad6d73e92c50083a39d461074a297b85605
Quentin Neal <quentinneal@outlook.com>
2022-11-22 20:08:28 +0000

    update

--- script.test.js
+++ script.test.js
@@ -1,3 +1,16 @@
-it('this is a test', () => {
+const googleSearch = require('./script');
 
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com',
+];
+
+it('failed search', () => {
+    expect(googleSearch('dis', dbMock)).toEqual([])
+})
+
+it('successful search', () => {
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
 })
